e30bc5aa120d5b4d8a2a21ffd3bd11357759d3e5
imdaegeun <cd3818@naver.com>
2021-11-16 21:48:24 +0900

    11/16 - 1sol

---
+++ DP/15990.py
@@ -0,0 +1,17 @@
+# 1,2,3 더하기 5
+dp = [[0]*4 for _ in range(100001)]
+dp[1] = [0, 1, 0, 0]
+dp[2] = [0, 0, 1, 0]
+dp[3] = [0, 1, 1, 1]
+for i in range(4, 100001):
+    for case in range(1, 4):
+        if case == 1:
+            dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % 1000000009
+        elif case == 2:
+            dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % 1000000009
+        else:
+            dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % 1000000009
+
+for _ in range(int(input())):
+    n = int(input())
+    print(sum(dp[n]) % 1000000009)
