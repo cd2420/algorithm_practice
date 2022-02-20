c8d8051de34daac14eff69a6f46f229f73dda620
imdaegeun <cd3818@naver.com>
2022-02-21 07:38:18 +0900

    2/21
    - 1197: add kruskal algorithm

--- 최단경로/1197.py
+++ 최단경로/1197.py
@@ -23,3 +23,36 @@ while q:
         if not visited[i[1]]:
             heapq.heappush(q, (i[0], i[1]))
 print(result)
+
+
+# 크루스칼 알고리즘
+def find_parent(x):
+    if x == tree[x]:
+        return x
+    else:
+        a = find_parent(tree[x])
+        tree[x] = a
+        return a
+
+
+v, e = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(e)]
+graph.sort(key=lambda x: x[2])
+tree = {}
+for i in range(1, v+1):
+    tree[i] = i
+
+result = 0
+for now, nxt, cost in graph:
+
+    a = find_parent(now)
+    b = find_parent(nxt)
+    if a == b:
+        continue
+    else:
+        result += cost
+        if a > b:
+            tree[a] = b
+        else:
+            tree[b] = a
+print(result)
