48877105fbee04cd14a95486ae9576a4ddbff4d4
imdaegeun <cd3818@naver.com>
2022-06-10 14:44:46 +0900

    6/10
    - 1 impl retry

--- Implementation/1655.py
+++ Implementation/1655.py
@@ -1,1 +1,25 @@
-# 가운데를 말해요
+# 가운데를 말해요 (다시)
+import heapq
+import sys
+input = sys.stdin.readline
+n = int(input())
+left = []
+right = []
+result = []
+for _ in range(n):
+    num = int(input())
+    if len(left) == len(right):
+        heapq.heappush(left, num * -1)
+    else :
+        heapq.heappush(right, num)
+    if right and left[0] * -1 > right[0]:
+        left_num = heapq.heappop(left) * -1
+        right_num = heapq.heappop(right)
+        heapq.heappush(right, left_num)
+        heapq.heappush(left, right_num * -1)
+    result.append(left[0] * -1)
+for r in result:
+    print(r)
+
+
+    
