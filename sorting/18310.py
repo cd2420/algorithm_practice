40656b4cee88af58d34972c3737b1b428b7b2fec
imdaegeun <cd3818@naver.com>
2022-07-12 20:26:25 +0900

    7/12
    - 1 sort retry sol

--- sorting/18310.py
+++ sorting/18310.py
@@ -1,22 +1,18 @@
 # 안테나
 n = int(input())
-anthena = list(map(int, input().split()))
-anthena.sort()
-size = len(anthena)
-if  size % 2 == 1:
-    print(anthena[size // 2])
-else :
-    check = -1
-    result = 0
-    for i in [size // 2 - 1, size // 2]:
-        tmp = 0
-        for j in range(n):
-            tmp += abs(anthena[j] - anthena[i])
-        if check == -1:
-            check = tmp
-            result = i
-        else :
-            if check > tmp:
-                check = tmp
-                result = i
-    print(anthena[result])
+antena = list(map(int, input().split()))
+antena.sort()
+size = len(antena)
+t = size // 2
+total = 0
+for a in antena:
+    total += abs(a - antena[t])
+result = antena[t]
+if size % 2 == 0:
+    t1 = t - 1
+    check = 0
+    for a in antena:
+        check += abs(a - antena[t1])
+    if total >= check:
+        result = antena[t1]
+print(result)
