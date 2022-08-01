d666f7d2e274bac5c2910e94e568cf8b969aa231
imdaegeun <cd3818@naver.com>
2022-08-01 23:12:04 +0900

    8/1
    1 dfs sol

--- BFS & DFS/travelRoute.py
+++ BFS & DFS/travelRoute.py
@@ -1,2 +1,21 @@
 # 프로그래머스
 # 여행경로
+from collections import defaultdict
+def solution(tickets):
+    result = len(tickets) + 1
+    trip = defaultdict(list)
+    for a,b in tickets:
+        trip[a].append(b)
+        trip[a].sort(reverse=True)
+    stack = ["ICN"]
+
+    tmp_answer = []
+    while stack:
+        now = stack[-1]
+        if not trip[now]:
+            tmp_answer.append(stack.pop())
+        else :
+            nxt = trip[now].pop()
+            stack.append(nxt)
+    answer = tmp_answer[::-1]
+    return answer
