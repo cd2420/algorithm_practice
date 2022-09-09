5469b38b6e02afce15e4b8f0a6ef6f5ca775caab
imdaegeun <cd3818@naver.com>
2022-09-09 18:33:47 +0900

    9/9
    - 1 이분탐색 sol

--- 이분탐색/searchSong.py
+++ 이분탐색/searchSong.py
@@ -2,29 +2,30 @@
 # 가사 검색
 from bisect import bisect_left, bisect_right
 from collections import defaultdict
-
-def check(words, left, right):
-    left_idx = bisect_left(words, left)
-    right_idx = bisect_right(words, right)
-    return right_idx - left_idx
+def find_song(arr, a, z):
+    return bisect_right(arr, z) - bisect_left(arr, a)
 
 def solution(words, queries):
     answer = []
-    dic = defaultdict(list)
-    reverse_dic = defaultdict(list)
+    wordList = defaultdict(list)
+    wordListRevers = defaultdict(list)
     for word in words:
-        dic[len(word)].append(word)
-        reverse_dic[len(word)].append(word[::-1])
-    
-    for key in dic.keys():
-        dic[key].sort()
-        reverse_dic[key].sort()
+        wordList[len(word)].append(word)
+        wordListRevers[len(word)].append(word[::-1])
         
-    for query in queries:
-        if query[0] != '?':
-            answer.append(check(dic[len(query)], query.replace("?", "a"), query.replace("?", "z")))
-        else :
-            query = query[::-1]
-            answer.append(check(reverse_dic[len(query)], query.replace("?", "a"), query.replace("?", "z")))
+    for key in wordList:
+        wordList[key].sort()
+        wordListRevers[key].sort()
     
+    for query in queries:
+        
+        cnt = 0
+        a_replace_word = query.replace("?", "a")
+        z_replace_word = query.replace("?", "z")
+        if query[0] != "?":
+            cnt = find_song(wordList[len(query)], a_replace_word, z_replace_word)
+        else :
+            cnt = find_song(wordListRevers[len(query)], a_replace_word[::-1], z_replace_word[::-1])
+        answer.append(cnt)
+        
     return answer
