ec27a2de68546d2993d6e0c69aaeb8fee5b168b0
imdaegeun <cd3818@naver.com>
2022-09-18 23:27:31 +0900

    크루스칼 알고리즘과 프림 알고리즘 차이점

--- 최단경로/1197.py
+++ 최단경로/1197.py
@@ -1,58 +1,61 @@
 # 최소 스패닝 트리 (최소 신장 트리)
-
-# 프림 알고리즘.
-import heapq
-
-v, e = map(int, input().split())
-graph = [[] for _ in range(v + 1)]
-for _ in range(e):
-    a, b, c = map(int, input().split())
-    graph[a].append((c, b))
-    graph[b].append((c, a))
-visited = [False] * (v + 1)
-q = [(0, 1)]
-result = 0
-while q:
-    cost, x = heapq.heappop(q)
-    if visited[x]:
-        continue
-    visited[x] = True
-
-    result += cost
-    for i in graph[x]:
-        if not visited[i[1]]:
-            heapq.heappush(q, (i[0], i[1]))
-print(result)
-
-
-# 크루스칼 알고리즘
+# 크루스칼 알고리즘.
 def find_parent(x):
-    if x == tree[x]:
+    if parent[x] == x:
         return x
-    else:
-        a = find_parent(tree[x])
-        tree[x] = a
+    else :
+        a = find_parent(parent[x])
+        parent[x] = a
         return a
 
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
 
-v, e = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(e)]
-graph.sort(key=lambda x: x[2])
-tree = {}
-for i in range(1, v+1):
-    tree[i] = i
+    if a > b:
+        parent[a] = b
+    else :
+        parent[b] = a
 
+V, E = map(int, input().split())
+parent = [i for i in range(V+1)]
+graph = []
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph.append((c, a, b))
+graph.sort()
 result = 0
-for now, nxt, cost in graph:
-
-    a = find_parent(now)
-    b = find_parent(nxt)
+for cnt, a, b in graph:
+    a = find_parent(a)
+    b = find_parent(b)
     if a == b:
         continue
-    else:
-        result += cost
-        if a > b:
-            tree[a] = b
-        else:
-            tree[b] = a
+    result += cnt
+    union_parent(a, b)
+print(result)
+
+# 프림 알고리즘.
+from collections import defaultdict
+import heapq
+
+V,E = map(int, input().split())
+graph = defaultdict(list)
+for _ in range(E):
+    a,b,c = map(int, input().split())
+    graph[a].append((c, a, b))
+    graph[b].append((c, b, a))
+
+connected_node = set()
+connected_node.add(1)
+candidate_edge_list = graph[1]
+heapq.heapify(candidate_edge_list)
+result = 0
+while candidate_edge_list:
+    cnt, a, b = heapq.heappop(candidate_edge_list)
+    if b not in connected_node:
+        connected_node.add(b)
+        result += cnt
+        for edge in graph[b]:
+            if edge[2] not in connected_node:
+                heapq.heappush(candidate_edge_list, edge)
 print(result)
