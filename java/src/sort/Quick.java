8db183430de48971f496557214dce23f316b14a5
imdaegeun <cd3818@naver.com>
2022-11-13 12:44:21 +0900

    java quick sort

---
+++ java/src/sort/Quick.java
@@ -0,0 +1,50 @@
+package sort;
+
+import static sort.Base.numsSetting;
+import static sort.Base.show;
+
+public class Quick {
+
+    public static void main(String[] args) {
+        int[] nums = numsSetting();
+        int x = nums.length;
+
+        show(nums);
+        quickSort(nums, 0, x-1);
+        show(nums);
+
+    }
+
+    private static void quickSort(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int pivot = start;
+        int left = start+1;
+        int right = end;
+
+        while (left <= right) {
+            while (left <= end && nums[pivot] >= nums[left]) {
+                left++;
+            }
+
+            while (right > start && nums[pivot] <= nums[right]) {
+                right--;
+            }
+
+            int temp;
+            if (left > right) {
+                temp = nums[pivot];
+                nums[pivot] = nums[right];
+            } else {
+                temp = nums[left];
+                nums[left] = nums[right];
+            }
+            nums[right] = temp;
+
+        }
+        quickSort(nums, start, right-1);
+        quickSort(nums, right+1, end);
+
+    }
+}
